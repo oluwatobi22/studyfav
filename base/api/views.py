cd7c04585fa67a99d51394591d22aecd0d7efad8
Oluwatobi22 <favouroluwatobiloba2002@gmail.com>
2024-04-29 20:14:19 +0100

    made changes

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,9 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import  Response 
+from base.models import Room
+from .serializers import RoomSerializer
 
+@api_view(['GET'])
 def getRoutes(request, *args, **kwargs):
   routes = [
     'GET /api',
@@ -7,4 +11,10 @@ def getRoutes(request, *args, **kwargs):
     'GET /api/rooms/:id'
   ]
 
-  return JsonResponse(routes, safe=False)
+  return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+  rooms = Room.objects.all()
+  serializer = RoomSerializer(rooms, many=True)
+  return Response(serializer.data)
