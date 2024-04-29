72e90477b84cb7834398452e179ace524dc3c80b
Oluwatobi22 <favouroluwatobiloba2002@gmail.com>
2024-04-29 20:40:26 +0100

    made changes

--- base/api/views.py
+++ base/api/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import  Response 
 from base.models import Room
@@ -18,3 +20,12 @@ def getRooms(request):
   rooms = Room.objects.all()
   serializer = RoomSerializer(rooms, many=True)
   return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+  try:
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
+  except Room.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
